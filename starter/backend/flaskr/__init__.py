b175db492c2165be134f5d6d7efa72138c2353fd
DoubleHelix <tjrnyc5@gmail.com>
2020-06-29 15:19:27 -0400

    completed delete question endpoint

--- starter/backend/flaskr/__init__.py
+++ starter/backend/flaskr/__init__.py
@@ -95,6 +95,27 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  
+  @app.route('/question/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    try:
+      question = Question.query.filter(Question.id == question_id).one_or_none()
+
+      if question is None:
+        abort(404)
+      question.delete()
+      selection = Question.query.order_by(Question.id).all()
+      current_questions = paginate_questions(request, selection)
+
+      return jsonify({
+      'success': True,
+      'deleted': question_id,
+      'categories': current_questions,
+      'total_questions': len(Question.query.all())
+      })
+
+    except:
+      abort(422)
 
   '''
   @TODO: 
@@ -106,6 +127,7 @@ def create_app(test_config=None):
   the form will clear and the question will appear at the end of the last page
   of the questions list in the "List" tab.  
   '''
+
 
   '''
   @TODO: 
