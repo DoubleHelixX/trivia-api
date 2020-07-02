e9efea2ec5c1967d7a76fd94613b8e87da5063ab
DoubleHelix <tjrnyc5@gmail.com>
2020-07-02 19:19:09 -0400

    Unit test - get paginated questions complete

--- starter/backend/test_flaskr.py
+++ starter/backend/test_flaskr.py
@@ -14,8 +14,9 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_name = "trivia"
+        self.database_path = "postgresql+psycopg2://{}:{}@{}/{}".format('postgres', '1','localhost:5432', self.database_name)
+
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,7 +34,102 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
 
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+    
+    # def test_get_paginated_categories(self):
+    #     res = self.client().get('/categories')
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertTrue(data['total_categories'])
+    #     self.assertTrue(len(data['categories']))
+    
+    # def test_404_sent_requesting_beyond_valid_page(self):
+    #     res = self.client().get('/questions?page=1000', json={'difficulty': 1})
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code, 404)
+    #     self.assertEqual(data['success'], False)
+    #     self.assertEqual(data['message'], 'resource not found')
+    
+    # # @TODO: Write tests for search - at minimum two
+    # #        that check a response when there are results and when there are none
+
+    # def test_update_book_rating(self):
+    #     res = self.client().patch('/books/5', json={'rating': 1})
+    #     data = json.loads(res.data)
+    #     book = Book.query.filter(Book.id == 5).one_or_none()
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertEqual(book.format()['rating'], 1)
+        
+
+    # def test_400_for_failed_update(self):
+    #     res = self.client().patch('/books/5')
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code, 400)
+    #     self.assertEqual(data['success'], False)
+    #     self.assertEqual(data['message'], 'bad request')
+    
+    # def test_delete_book(self):
+    #     res = self.client().delete('/books/6')
+    #     data = json.loads(res.data)
+
+    #     book = Book.query.filter(Book.id == 6).one_or_none()
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertEqual(data['deleted'], 6)
+    #     self.assertTrue(data['total_books'])
+    #     self.assertTrue(len(data['books']))
+    #     self.assertEqual(book, None)
+        
+
+    # def test_404_if_book_does_not_exist(self):
+    #     res = self.client().delete('/books/1000')
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code, 422)
+    #     self.assertEqual(data['success'], False)
+    #     self.assertEqual(data['message'], 'unprocessable')
+    
+    # def test_create_new_book(self):
+    #     res = self.client().post('/books', json=self.new_book)
+    #     data = json.loads(res.data)
+    #     pass
+    
+    # def test_422_if_book_creation_fails(self):
+    #     res = self.client().post('/books', json=self.new_book)
+    #     data = json.loads(res.data)
+    #     pass
+    
+    # def test_get_book_search_with_results(self):
+    #     res = self.client().post('/books', json={'search': 'Novel'})
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertTrue(data['total_books'])
+    #     self.assertEqual(len(data['books']), 4)
+
+    # def test_get_book_search_without_results(self):
+    #     res = self.client().post('/books', json={'search': 'applejacks'})
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertEqual(data['total_books'], 0)
+    #     self.assertEqual(len(data['books']), 0)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
